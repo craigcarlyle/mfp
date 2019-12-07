644c35712f106ff683eb9ade98da5008e3a4084e
Craig Carlyle <craig.carlyle@clio.com>
2019-12-07 11:53:57 -0800

    Cleanup

--- mfp_functions/fetchDateRange.js
+++ mfp_functions/fetchDateRange.js
@@ -1,17 +1,16 @@
-'use strict';
+"use strict";
 
-var request = require('request');
-var cheerio = require('cheerio');
-var helpers = require('./helper-utils');
+var request = require("request");
+var cheerio = require("cheerio");
+var helpers = require("./helper-utils");
 
-var fetchDateRange = function(username, startDate, endDate, fields, callback){
+var fetchDateRange = function(username, startDate, endDate, fields, callback) {
   //get MyFitnessPal URL (eg. 'https://www.myfitnesspal.com/reports/printable_diary/npmmfp?from=2014-09-13&to=2014-09-17')
   var url = helpers.mfpUrl(username, startDate, endDate);
 
   var options = {
     url: url,
-    headers: {
-    }
+    headers: {}
   };
 
   request(options, function(error, response, body) {
@@ -25,11 +24,14 @@ var fetchDateRange = function(username, startDate, endDate, fields, callback){
 
     //for each date encountered, add the date formatted as YYYY-MM-DD and data table
     //to their respective arrays
-    $('.main-title-2').each(function(index, element){
+    $(".main-title-2").each(function(index, element) {
       var $element = $(element);
-      dates.push( helpers.formatDate(new Date($element.text())) );
-      $tables.push($element.next('#food'));
-      var exerciseTable = $element.next('#food').length > 0 ? $element.next().next() : $element.next("#excercise");
+      dates.push(helpers.formatDate(new Date($element.text())));
+      $tables.push($element.next("#food"));
+      var exerciseTable =
+        $element.next("#food").length > 0
+          ? $element.next().next()
+          : $element.next("#excercise");
       $excerciseTables.push(exerciseTable);
     });
 
@@ -45,18 +47,30 @@ var fetchDateRange = function(username, startDate, endDate, fields, callback){
       var results = {
         nutrition: {},
         workouts: [],
-        caffeine: [],
+        caffeine: {
+          coffee: 0,
+          tea: 0,
+          preworkout: 0
+        }
       };
 
       //define variable for determining columns of fields on MFP page
       var cols = {
         nutrition: {},
         workouts: [],
-        caffeine: [],
+        caffeine: {
+          coffee: 0,
+          tea: 0,
+          preworkout: 0
+        }
       };
 
       $table.find("td:contains('Coffee')").each(function(index, element) {
-        var cupsOfCoffee = Number($(element).text().charAt(15));
+        var cupsOfCoffee = Number(
+          $(element)
+            .text()
+            .charAt(15)
+        );
         if (results.caffeine.coffee) {
           results.caffeine.coffee += cupsOfCoffee;
         } else {
@@ -64,15 +78,20 @@ var fetchDateRange = function(username, startDate, endDate, fields, callback){
         }
       });
 
-
-      $table.find("td:contains('Green Tea - Green Tea')").each(function(index, element) {
-        var cupsOfTea = Number($(element).text().charAt(23));
-        if (results.caffeine.tea) {
-          results.caffeine.tea += cupsOfTea;
-        } else {
-          results.caffeine.tea = cupsOfTea;
-        }
-      });
+      $table
+        .find("td:contains('Green Tea - Green Tea')")
+        .each(function(index, element) {
+          var cupsOfTea = Number(
+            $(element)
+              .text()
+              .charAt(23)
+          );
+          if (results.caffeine.tea) {
+            results.caffeine.tea += cupsOfTea;
+          } else {
+            results.caffeine.tea = cupsOfTea;
+          }
+        });
 
       $table.find("td:contains('C4')").each(function(index, element) {
         if (results.caffeine.preworkout) {
@@ -83,48 +102,77 @@ var fetchDateRange = function(username, startDate, endDate, fields, callback){
       });
 
       //find and set column numbers of nutrient fields
-      $table.find('thead').find('tr').find('td').each(function(index, element){
-        var $element = $(element);
-        var fieldName = $element.text().toLowerCase();
-        if (fieldName === "sugars") { fieldName = "sugar"; } // fixes MFP nutrient field name inconsistency
-        if (fieldName === "cholest") { fieldName = "cholesterol"; } // fixes MFP nutrient field name inconsistency
-        if (index !== 0) { cols.nutrition[fieldName] = index; } //ignore first field, which just says "Foods"
-      });
+      $table
+        .find("thead")
+        .find("tr")
+        .find("td")
+        .each(function(index, element) {
+          var $element = $(element);
+          var fieldName = $element.text().toLowerCase();
+          if (fieldName === "sugars") {
+            fieldName = "sugar";
+          } // fixes MFP nutrient field name inconsistency
+          if (fieldName === "cholest") {
+            fieldName = "cholesterol";
+          } // fixes MFP nutrient field name inconsistency
+          if (index !== 0) {
+            cols.nutrition[fieldName] = index;
+          } //ignore first field, which just says "Foods"
+        });
 
-      $excerciseTable.find('thead').find('tr').find('td').each(function(index, element){
-        var $element = $(element);
-        var fieldName = $element.text().toLowerCase();
-        if (index !== 0) { cols.workouts[fieldName] = index; } //ignore first field, which just says "Foods"
-      });
+      $excerciseTable
+        .find("thead")
+        .find("tr")
+        .find("td")
+        .each(function(index, element) {
+          var $element = $(element);
+          var fieldName = $element.text().toLowerCase();
+          if (index !== 0) {
+            cols.workouts[fieldName] = index;
+          } //ignore first field, which just says "Foods"
+        });
 
       //find row in MFP with nutrient totals
-      var $nutritionDataRow = $table.find('tfoot').find('tr');
-      var $workoutsDataRow = $excerciseTable.find('tfoot').find('tr');
+      var $nutritionDataRow = $table.find("tfoot").find("tr");
+      var $workoutsDataRow = $excerciseTable.find("tfoot").find("tr");
 
-      $excerciseTable.find("tbody").find('tr:not(.title)').each(function(index, element){
-        var $element = $(element);
+      $excerciseTable
+        .find("tbody")
+        .find("tr:not(.title)")
+        .each(function(index, element) {
+          var $element = $(element);
 
-        if ($element.find("td:first-of-type").text() !== "MFP iOS calorie adjustment") {
-          var excerciseData = {
-            name: $element.find("td:first-of-type").text(),
-            calories: helpers.convertToNum($element.find("td:nth-of-type(2)").text()),
-            minutes: helpers.convertToNum($element.find("td:nth-of-type(3)").text())
+          if (
+            $element.find("td:first-of-type").text() !==
+            "MFP iOS calorie adjustment"
+          ) {
+            var excerciseData = {
+              name: $element.find("td:first-of-type").text(),
+              calories: helpers.convertToNum(
+                $element.find("td:nth-of-type(2)").text()
+              ),
+              minutes: helpers.convertToNum(
+                $element.find("td:nth-of-type(3)").text()
+              )
+            };
+            results.workouts.push(excerciseData);
           }
-          results.workouts.push(excerciseData);
-        }
-      });
+        });
 
       //store data for each field in results
       for (var field in cols.nutrition) {
         var col = cols.nutrition[field] + 1; //because nth-child selector is 1-indexed, not 0-indexed
-        var mfpData = $nutritionDataRow.find('td:nth-child(' + col + ')').first().text();
+        var mfpData = $nutritionDataRow
+          .find("td:nth-child(" + col + ")")
+          .first()
+          .text();
         results.nutrition[field] = helpers.convertToNum(mfpData);
       }
 
-      if (fields !== 'all' && Array.isArray(fields)) {
+      if (fields !== "all" && Array.isArray(fields)) {
         //create targetFields object to hash user-specified nutrient fields
         var targetFields = {};
-        fields.forEach(function(field){
+        fields.forEach(function(field) {
           targetFields[field] = true;
         });
 
@@ -143,10 +191,10 @@ var fetchDateRange = function(username, startDate, endDate, fields, callback){
 
     //iterate through all dates and push data into a final results object
     for (var i = 0; i < dates.length; i++) {
-      results.data.push( processDate(dates[i], $tables[i], $excerciseTables[i]) );
+      results.data.push(processDate(dates[i], $tables[i], $excerciseTables[i]));
     }
 
-    callback( results );
+    callback(results);
   });
 };
 
